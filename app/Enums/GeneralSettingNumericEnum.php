dd6164823319743d3693ce6a6f70bcee8e20dae7
rayanMaster <rayan@mstrz.com>
2024-11-26 20:15:53 +0300

    Remove all files from Git index

--- app/Enums/GeneralSettingNumericEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum GeneralSettingNumericEnum: int
-{
-    case PER_PAGE = 5;
-    case TTL = 3600;
-}
--- tests/Unit/ArchTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-arch('test security of application')->preset()->security();
-arch('test php preset restrictions of application')->preset()->php();
-//arch('test Laravel preset restrictions of application')->preset()->laravel();
-//arch('test Relaxed preset restrictions of application')->preset()->relaxed();
-//arch('test Strict preset restrictions of application')->preset()->strict();
